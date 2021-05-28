17669763fc82adb8b28c8d24dd2fcd5897989e7b
coderg1607 <71881776+coderg1607@users.noreply.github.com>
2021-05-28 14:52:43 +0530

    Update Chat.js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -30,7 +30,7 @@ const Chat = ({ location }) => {
         alert(error);
       }
     });
-  }, [ location.search]);
+  }, [ENDPOINT,location.search]);
   
   useEffect(() => {
     socket.on('message', message => {
