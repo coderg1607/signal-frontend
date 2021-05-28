4effa9545f3ff9bf82d5c6d0b572120f7e2a2a83
coderg1607 <71881776+coderg1607@users.noreply.github.com>
2021-05-28 14:50:51 +0530

    Update Chat.js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,7 +7,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+const ENDPOINT = 'https://signal-app-back.herokuapp.com/';
 
 let socket;
 
